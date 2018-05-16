870eb019072bde46beec863c78f9dc71b652048d
aminiconi <aminiconi@kangourouge.com>
2018-05-16 12:07:43 +0200

    [U] Remove async defer

--- Helper/ApiHelper.php
+++ Helper/ApiHelper.php
@@ -26,7 +26,7 @@ class ApiHelper
     public function render($language = 'en', array $libraries = array(), $callback = null)
     {
         $libraries = implode(',', $this->libraries);
-        $output = sprintf('<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=%s&libraries=%s&callback=%s" async defer></script>',
+        $output = sprintf('<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=%s&libraries=%s&callback=%s"></script>',
           $this->apiKey,
           $libraries,
           $callback);
